58644925f83df48f7a7daad9484acc37b4775d53
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-06-14 17:00:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function returnCustomerName() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function OverwriteBestCustomer() {
+    bestcustomer = 'new best customer';
+}
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Jane';
+}
